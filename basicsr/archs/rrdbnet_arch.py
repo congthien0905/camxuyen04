1d91412ed216415bb3e771bc0703175cd21d2450
congthien0905 <congthien.en@gmail.com>
2024-10-07 16:57:25 +0700

    real-esrgan 1.8

--- basicsr/archs/rrdbnet_arch.py
+++ basicsr/archs/rrdbnet_arch.py
@@ -36,7 +36,7 @@ class ResidualDenseBlock(nn.Module):
         x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
         x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
         # Empirically, we use 0.2 to scale the residual for better performance
-        return x5 * 0.14 + x
+        return x5 * 0.18 + x
 
 
 class RRDB(nn.Module):
@@ -60,7 +60,7 @@ class RRDB(nn.Module):
         out = self.rdb2(out)
         out = self.rdb3(out)
         # Empirically, we use 0.2 to scale the residual for better performance
-        return out * 0.14 + x
+        return out * 0.18 + x
 
 
 @ARCH_REGISTRY.register()
